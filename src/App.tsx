593713eaccf0059471ec3c51a985f6751e2d294e
leegain1 <gain3004@gmail.com>
2025-09-24 00:13:47 +0900

    feat : 버튼 컴포넌트 생성 및 tailwind.config.js 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import SplashScreen from './pages/splash/SplashScreen';
 import LoginPage from './pages/login/LoginPage';
 import ChatbotPage from './pages/chat/ChatbotPage';
 import AppLayout from './components/common/AppLayout';
+import RecipePage from '@/pages/receipt/RecipePage';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<SplashScreen />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/chat" element={<ChatbotPage />} />
+        <Route path="/recipe" element={<RecipePage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AppLayout>
