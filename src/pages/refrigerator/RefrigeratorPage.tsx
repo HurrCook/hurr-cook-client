afc4b6217fb9ba2b2a26410c028fadc0a4b9b876
이승준 <dltmdwns9645@gmail.com>
2025-11-14 04:02:58 +0900

    ㅌ

--- src/pages/refrigerator/RefrigeratorPage.tsx
+++ src/pages/refrigerator/RefrigeratorPage.tsx
@@ -194,7 +194,10 @@ export default function RefrigeratorPage() {
             <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
               {ingredients.length > 0 ? (
                 ingredients.map((item, index) => {
-                  const isExpired = new Date(item.expireDate) < new Date(); // ✅ 유통기한 비교
+                  const expire = new Date(item.expireDate);
+                  const isExpired =
+                    expire.getTime() + 9 * 3600 * 1000 <
+                    Date.now() + 9 * 3600 * 1000;
                   let imageSrc = isExpired ? DefaultBadUrl : DefaultGoodUrl; // ✅ 기본 이미지 조건 분기
 
                   if (item.imageUrl) {
