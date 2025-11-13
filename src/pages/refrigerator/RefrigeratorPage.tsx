288e0760774351f6c32537dea26cc06a7a0b0691
gain <163835628+leegain1@users.noreply.github.com>
2025-11-14 03:30:54 +0900

    fix : 폰 날짜 인식안하는거 수정

--- src/pages/refrigerator/RefrigeratorPage.tsx
+++ src/pages/refrigerator/RefrigeratorPage.tsx
@@ -194,7 +194,10 @@ export default function RefrigeratorPage() {
             <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
               {ingredients.length > 0 ? (
                 ingredients.map((item, index) => {
-                  const isExpired = new Date(item.expireDate) < new Date(); // ✅ 유통기한 비교
+                  const dateStr = item.expireDate
+                    .replace(/\./g, '-')
+                    .replace(/\s/g, '');
+                  const isExpired = new Date(dateStr) < new Date();
                   let imageSrc = isExpired ? DefaultBadUrl : DefaultGoodUrl; // ✅ 기본 이미지 조건 분기
 
                   if (item.imageUrl) {
