483418acf9cabbafd6c5c10ce6a845db11c3c093
gain <163835628+leegain1@users.noreply.github.com>
2025-11-13 23:34:53 +0900

    fix : vercel.json

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,10 @@
-// Completely remove previous PWA service worker
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  self.registration.unregister();
-  return self.clients.claim();
+
+self.addEventListener('activate', async () => {
+  // ❌ 기존 서비스 워커 제거
+  await self.registration.unregister();
+
+  // ❗️모든 탭 새로고침(중요)
+  const clients = await self.clients.matchAll();
+  clients.forEach((client) => client.navigate(client.url));
 });
