67731f1001e3e20bf33b3313319831b37665dd98
gain <163835628+leegain1@users.noreply.github.com>
2025-11-13 23:27:11 +0900

    feat : sw.js 추가

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+// Remove old service worker
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  self.clients.claim();
+});
