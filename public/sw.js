3cb2e511cb692ff31b96f3f2cb078bbcaf8629fa
gain <163835628+leegain1@users.noreply.github.com>
2025-11-13 23:54:30 +0900

    fix : vercel.json복원

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
+// 완전히 서비스워커 제거 (가장 강력한 죽이기 코드)
 self.addEventListener('install', () => self.skipWaiting());
-
 self.addEventListener('activate', async () => {
-  // ❌ 기존 서비스 워커 제거
+  // 자기 자신을 unregister
   await self.registration.unregister();
 
-  // ❗️모든 탭 새로고침(중요)
+  // 모든 클라이언트에게 페이지 새로고침 강제
   const clients = await self.clients.matchAll();
   clients.forEach((client) => client.navigate(client.url));
 });
